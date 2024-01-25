a7ba53734286af7d677442d09368cb25e8845740
Jörg Richter <jri@deepamehta.de>
2024-01-25 14:52:59 -0600

    Setup project

---
+++ src/main/java/systems/dmx/zwmigrator/migrations/Migration1.java
@@ -0,0 +1,22 @@
+package systems.dmx.zwmigrator.migrations;
+
+import static systems.dmx.zukunftswerk.Constants.*;
+
+import systems.dmx.core.service.Migration;
+
+
+
+/**
+ * Transforms a Zukunftswerk instance into a Linqa instance.
+ * <p>
+ * Runs ALWAYS.
+ */
+public class Migration1 extends Migration {
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+    @Override
+    public void run() {
+        // TODO
+    }
+}
