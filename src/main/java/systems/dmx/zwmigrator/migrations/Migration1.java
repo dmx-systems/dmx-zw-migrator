82ab6de9d03746b2b611a00feea4fb3fcbbd07ff
Jörg Richter <jri@deepamehta.de>
2024-01-26 13:52:02 -0600

    Transform Plugin topic

--- src/main/java/systems/dmx/zwmigrator/migrations/Migration1.java
+++ src/main/java/systems/dmx/zwmigrator/migrations/Migration1.java
@@ -1,6 +1,8 @@
 package systems.dmx.zwmigrator.migrations;
 
+import static systems.dmx.core.Constants.*;
 import static systems.dmx.zukunftswerk.Constants.*;
+import systems.dmx.core.Topic;
 
 import systems.dmx.core.service.Migration;
 
@@ -17,6 +19,13 @@ public class Migration1 extends Migration {
 
     @Override
     public void run() {
-        // TODO
+        // Plugin topic
+        Topic pluginTopic = dmx.getTopicByUri(ZW_PLUGIN_URI);
+        pluginTopic.update(mf.newChildTopicsModel()
+            .set(PLUGIN_NAME, "DMX Linqa")
+            .set(PLUGIN_SYMBOLIC_NAME, "systems.dmx.linqa")
+            .set(PLUGIN_MIGRATION_NR, 2)
+        );
+        pluginTopic.setUri("systems.dmx.linqa");
     }
 }
