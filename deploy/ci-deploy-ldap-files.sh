8be730401a7b80beb65cb71bb032c6382c2e1699
Juergen Neumann <juergen@junes.eu>
2024-02-08 10:45:46 +0100

    fix ldap

--- deploy/ci-deploy-ldap-files.sh
+++ deploy/ci-deploy-ldap-files.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 if [ -f deploy/ldap/ldap-cfg.tar.gz ]; then
+    if [ -d deploy/ldap/ldap-cfg ]; then
+        rm -rf deploy/ldap/ldap-cfg
+    fi
     tar -xzvf deploy/ldap/ldap-cfg.tar.gz -C deploy/ldap/
     rm deploy/ldap/ldap-cfg.tar.gz
 fi
 
 if [ -f deploy/ldap/ldap-db.tar.gz ]; then
+    if [ -d deploy/ldap/ldap-db ]; then
+        rm -rf deploy/ldap/ldap-db
+    fi
     tar -xzvf deploy/ldap/ldap-db.tar.gz -C deploy/ldap/
     rm deploy/ldap/ldap-db.tar.gz
 fi
